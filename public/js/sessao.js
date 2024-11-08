843d21c9d5d926ec99465e5c1ee995a3bc468692
israel-coaquira-jumpiri <israel.jumpiri@sptech.school>
2024-11-07 22:56:09 -0300

    Cadastro e login funcionando com banco de dados

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -6,9 +6,9 @@ function validarSessao() {
     var b_usuario = document.getElementById("b_usuario");
 
     if (email != null && nome != null) {
-        b_usuario.innerHTML = nome;
+        b_usuario.innerHTML = nome.toUpperCase();
     } else {
-        window.location = "../login.html";
+        window.location = "./login.html";
     }
 }
 
@@ -16,21 +16,3 @@ function limparSessao() {
     sessionStorage.clear();
     window.location = "../login.html";
 }
-
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
-
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
-
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
-}
-
