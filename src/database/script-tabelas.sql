bf1aa90ba51bb8054bd89c6c8252c81aaed6cab8
israel-coaquira-jumpiri <israel.jumpiri@sptech.school>
2024-11-09 17:27:56 -0300

    Layout de Sessao de usuário finalizada

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,19 +1,12 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
 CREATE DATABASE undereflexion;
 
 USE undereflexion;
 
 CREATE TABLE Usuario(
-	idUsuario INT,
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45),
     email VARCHAR(60),
     senha VARCHAR(45)
 );
+
 select * from Usuario;
